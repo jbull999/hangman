1b5ed88b7865c9b434421c32d58ee3980df86d58
Jade Bullock <jadebull999@gmail.com>
2025-11-11 16:06:58 +0100

    added testing file and reversed hangman

--- Hangman_game.py
+++ Hangman_game.py
@@ -19,52 +19,54 @@ def main():
     or the hangman drawing is finished (8 wrong guesses)."""
 
     def get_word():
-        print("Welcome to the Hangman Game!")
-        print("This is a 2 player game, where one player defines a secret word and the other player guesses the secret word.")
-        print("Player 1 - Think of a secret word.")
         word = input("Enter secret word...").upper()
-
+        return word.upper()
 
     def play(word):
+        print("\nWelcome to the Hangman Game!")
+        print("\nLets Play!")
+        print("\nThis is a 2 player game, where one player defines a secret word and the other player guesses the secret word.")
+        print("\nPlayer 1 - Think of a secret word.")
+        get_word()
         word_completion = "_" * len(word)
         guessed = False
         guessed_letters = []
         guessed_words = []
         tries = 8
+        print(word_completion)
+        print("Player 2 - You have ", tries, "more guesses.")
+        while not guessed and tries > 0:
+            guess = input("\nGuess a letter in the secret word").upper()
+            if guess in guessed_letters:
+                print("\nYou already guessed that letter.")
+            elif guess not in word:
+                print (guess, "\n is not in the secret word.")
+                guessed_letters.append(guess)
+                tries -= 1
+                print(Hangman(tries))
 
 
+            #else:
+                #print("_", end="")
+                    #failed += 1
+            #if failed == 0:
+                #print("You win!  You guessed the secret word!")
+                #break
 
-    guesses = ""
-    guesses += guess.upper()
+    #if guess not in word:
+        #print("Sorry, that letter is not in the secret word.")
+        #tries -= 1
+        #print("Player 2 - You have ", turn, "more guesses.")
 
-    tries = 8
-    while tries > 0:
-        failed = 0
-
-        for char in word:
-            if char in guesses:
-                print(char, end="")
-            else:
-                print("_", end="")
-                failed += 1
-            if failed == 0:
-                print("You win!  You guessed the secret word!")
-                break
-
-    if guess not in word:
-        print("Sorry, that letter is not in the secret word.")
-        tries -= 1
-        print("Player 2 - You have ", turn, "more guesses.")
-
-        if guesses == 0:
-            print("Player 2: You lose!  You did not guess the secret word!")
+        #if guesses == 0:
+            #print("Player 2: You lose!  You did not guess the secret word!")
 
 
-    while guesses not in word:
-        print
+    #while guesses not in word:
+        #print
 
 # Hangman ASCII art stages (from 0 mistakes to 6 mistakes)
-def Hangman():
+def Hangman(tries):
     HANGMAN_PICS = [
         """ 
         """,
@@ -134,9 +136,8 @@ def Hangman():
         """
     ]
 
-# Example usage
-mistakes = 3
-print(HANGMAN_PICS[mistakes])
+
+    return HANGMAN_PICS[tries]
 
 
 
---
+++ working.py
@@ -0,0 +1,120 @@
+def get_word():
+    print("\nWelcome to the Hangman Game!")
+    print("\nLets Play!")
+    print("\nThis is a 2 player game, where one player defines a secret word and the other player guesses the secret word.")
+    print("\nPlayer 1 - Think of a secret word.")
+    word = input("Enter secret word...").upper()
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 8
+    print(word_completion)
+    print("Player 2 - You have ", tries, "more guesses.")
+    while not guessed and tries > 0:
+        guess = input("\nGuess a letter in the secret word").upper()
+        if guess in guessed_letters:
+            print("\nYou already guessed that letter.")
+        elif guess not in word:
+            print(guess, "\n is not in the secret word.")
+            guessed_letters.append(guess)
+            tries -= 1
+            print(Hangman(tries))
+
+            # else:
+            # print("_", end="")
+            # failed += 1
+            # if failed == 0:
+            # print("You win!  You guessed the secret word!")
+            # break
+
+    # if guess not in word:
+    # print("Sorry, that letter is not in the secret word.")
+    # tries -= 1
+    # print("Player 2 - You have ", turn, "more guesses.")
+
+    # if guesses == 0:
+    # print("Player 2: You lose!  You did not guess the secret word!")
+
+    # while guesses not in word:
+    # print
+
+
+# Hangman ASCII art stages (from 0 mistakes to 6 mistakes)
+def Hangman(tries):
+    HANGMAN_PICS = [
+        """ 
+        """,
+        """
+         +---+
+             |
+             |
+             |
+             |
+            ===
+        """,
+        """
+         +---+
+             |
+         O   |
+             |
+             |
+            ===
+        """,
+        """
+         +---+
+             |
+         O   |
+         |   |
+             |
+            ===
+        """,
+        """
+         +---+
+             |
+         O   |
+        /|   |
+             |
+            ===
+        """,
+        """
+         +---+
+             |
+         O   |
+        /|\  |
+             |
+            ===
+        """,
+        """
+         +---+
+             |  
+         O   |
+        /|\  |
+        /    |
+            ===
+        """,
+        """
+         +---+
+             |    
+         O   |
+        /|\  |
+        / \  |
+            ===
+        """,
+        """
+         +---+
+         |   |
+         O   |
+        /|\  |
+        / \  |
+            ===
+        """
+    ]
+
+    return HANGMAN_PICS[::-1][tries]
+
+play(get_word())
+
